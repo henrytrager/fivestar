34698c5467d3b9003dbc9ccb5abce3003e344014
Matt Condon <MattCMultimedia@gmail.com>
2014-03-10 16:16:53 -0500

    perfected loading indicator showing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,19 +2,29 @@
 
 angular.module('fivestarApp')
 .controller('MainCtrl', function ($scope, Search, debounce, $routeParams, $location) {
-    $scope.index = $routeParams.index || 'none';
-    $scope.query = $routeParams.query || '';
-    $scope.node = $routeParams.node || undefined;
-    $scope.brand = $routeParams.brand || undefined;
+
+    $scope.loading = false;
 
     $scope.updateURL = function() {
         $location.search('query', $scope.query);
         $location.search('index', $scope.index);
         $location.search('node', $scope.node);
         $location.search('brand', $scope.brand);
+        $scope.getData();
     };
 
     $scope.getData = debounce(400, function() {
+        $scope.results = undefined;
+        $scope.loading = true;
+        if ($scope.query.length <= 0) {
+            $scope.loading = false;
+            return;
+        }
+        if ($scope.index === 'none') {
+            // alert or something to show them they need to select a thing
+            $scope.loading = false;
+            return;
+        }
         $scope.results = Search.get({
             query: $scope.query,
             index: $scope.index,
@@ -23,26 +33,17 @@ angular.module('fivestarApp')
         });
     });
 
-    $scope.$on('$routeChangeSuccess', function() {
-        // $scope.index = $routeParams.index || 'none';
-        // $scope.query = $routeParams.query || '';
-        // $scope.node = $routeParams.node || undefined;
-        // $scope.brand = $routeParams.brand || undefined;
-        if ($scope.query.length <= 0) {
-            return;
-        }
-        if ($scope.index === 'none') {
-            // alert or something to show them they need to select a thing
-            return;
-        }
-        $scope.getData();
-
-    });
+    $scope.$on('$routeChangeSuccess', $scope.getData);
 
     $scope.$watch('query', $scope.updateURL);
     $scope.$watch('index', $scope.updateURL);
     $scope.$watch('node', $scope.updateURL);
     $scope.$watch('brand', $scope.updateURL);
+
+    $scope.index = $routeParams.index || 'none';
+    $scope.query = $routeParams.query || '';
+    $scope.node = $routeParams.node || undefined;
+    $scope.brand = $routeParams.brand || undefined;
 
 
     // NODE
@@ -56,5 +57,7 @@ angular.module('fivestarApp')
         } else if (params.Name === 'BrowseNode') {
             $scope.node = params.Value;
         }
+
+
     };
 });
