67e4598f674830ac1df9a599be321820086351be
Matt Condon <MattCMultimedia@gmail.com>
2014-03-10 15:10:46 -0500

    got url wokring, but it reload on change, which is not desired

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('fivestarApp')
-.controller('MainCtrl', function ($scope, Search, debounce, $routeParams) {
+.controller('MainCtrl', function ($scope, Search, debounce, $routeParams, $location) {
     $scope.index = $routeParams.index || 'none';
     $scope.query = $routeParams.query || '';
     $scope.node = $routeParams.node || undefined;
     $scope.brand = $routeParams.brand || undefined;
 
-    $scope.updateSearch = debounce(400, function() {
-        if ($scope.query.length <= 0) {
-            return;
-        }
-        if ($scope.index === 'none') {
-            // alert or something to show them they need to select a thing
-            return;
-        }
+    $scope.updateURL = function() {
+        $location.search('query', $scope.query);
+        $location.search('index', $scope.index);
+        $location.search('node', $scope.node);
+        $location.search('brand', $scope.brand);
+    };
+
+    $scope.getData = debounce(400, function() {
         $scope.results = Search.get({
             query: $scope.query,
             index: $scope.index,
@@ -28,15 +28,17 @@ angular.module('fivestarApp')
         // $scope.query = $routeParams.query || '';
         // $scope.node = $routeParams.node || undefined;
         // $scope.brand = $routeParams.brand || undefined;
-        debugger;
-        $scope.results = Search.get({
-            query: $scope.query,
-            index: $scope.index,
-            node: $scope.node,
-            brand: $scope.brand
-        });
+        if ($scope.query.length <= 0) {
+            return;
+        }
+        if ($scope.index === 'none') {
+            // alert or something to show them they need to select a thing
+            return;
+        }
+        $scope.getData();
+
     });
 
-    // $scope.$watch('query', $scope.updateSearch);
-    // $scope.$watch('index', $scope.updateSearch);
+    $scope.$watch('query', $scope.updateURL);
+    $scope.$watch('index', $scope.updateURL);
 });
