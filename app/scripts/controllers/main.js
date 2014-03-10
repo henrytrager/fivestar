d41a4224c8eaf5df5df677dad15884302b2c367d
Matt Condon <MattCMultimedia@gmail.com>
2014-03-10 14:47:44 -0500

    imported routeparams

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('fivestarApp')
-.controller('MainCtrl', function ($scope, Search, debounce) {
+.controller('MainCtrl', function ($scope, Search, debounce, $routeParams) {
     $scope.index = 'none';
     $scope.query = '';
 
