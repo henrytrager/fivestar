0454bac6e220ee3296f57071c85e494a241d63ec
Matt Condon <MattCMultimedia@gmail.com>
2014-03-10 22:44:28 -0500

    implemented clear buttons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,19 @@
 angular.module('fivestarApp')
 .controller('MainCtrl', function ($scope, Search, debounce, $routeParams, $location) {
 
+
+    $scope.index = $routeParams.index || 'All';
+    $scope.query = $routeParams.query || '';
+    $scope.node = $routeParams.node || undefined;
+    $scope.brand = $routeParams.brand || undefined;
+
     $scope.loading = false;
 
-    $scope.updateURL = function() {
+    $scope.updateURL = function(clearOthers) {
+        clearOthers = clearOthers === true ? true :false;
+        $scope.node = clearOthers ? undefined : $scope.node;
+        $scope.brand = clearOthers ? undefined : $scope.brand;
+
         $location.search('query', $scope.query);
         $location.search('index', $scope.index);
         $location.search('node', $scope.node);
@@ -20,11 +30,11 @@ angular.module('fivestarApp')
             $scope.loading = false;
             return;
         }
-        if ($scope.index === 'none') {
-            // alert or something to show them they need to select a thing
-            $scope.loading = false;
-            return;
-        }
+        // if ($scope.index === 'none') {
+        //     // alert or something to show them they need to select a thing
+        //     $scope.loading = false;
+        //     return;
+        // }
         $scope.results = Search.get({
             query: $scope.query,
             index: $scope.index,
@@ -36,14 +46,11 @@ angular.module('fivestarApp')
     $scope.$on('$routeChangeSuccess', $scope.getData);
 
     $scope.$watch('query', $scope.updateURL);
-    $scope.$watch('index', $scope.updateURL);
+    $scope.$watch('index', function() {
+        $scope.updateURL(true);
+    });
     $scope.$watch('node', $scope.updateURL);
     $scope.$watch('brand', $scope.updateURL);
-
-    $scope.index = $routeParams.index || 'none';
-    $scope.query = $routeParams.query || '';
-    $scope.node = $routeParams.node || undefined;
-    $scope.brand = $routeParams.brand || undefined;
 
 
     // NODE
@@ -56,18 +63,18 @@ angular.module('fivestarApp')
     };
 
     $scope.isBin = function(bin) {
-        if (bin === 'Brand') {
+        if (bin === 'BrandName') {
             return $scope.brand !== undefined;
-        } else if (bin === 'BrowseNode') {
+        } else if (['Categories', 'Subject'].indexOf(bin) !== -1) {
             return $scope.node !== undefined;
         }
     };
 
     $scope.clearBin = function(bin) {
-        if (bin.Name === 'Brand') {
-            delete $scope.brand;
-        } else if (bin.Name === 'BrowseNode') {
-            delete $scope.node;
+        if (bin === 'BrandName') {
+            $scope.brand = undefined;
+        } else if (['Categories', 'Subject'].indexOf(bin) !== -1) {
+            $scope.node = undefined;
         }
     };
 });
