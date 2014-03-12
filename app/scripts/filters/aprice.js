7ab1c76274269eb8595ea6cf2a285f7cb9555551
Matt Condon <MattCMultimedia@gmail.com>
2014-03-11 21:27:06 -0500

    added plus sign to minPrices

--- app/scripts/filters/aprice.js
+++ app/scripts/filters/aprice.js
@@ -6,6 +6,6 @@ angular.module('fivestarApp')
         // if (input.maxPrice === 'max') {
         //     return '$' + (window.parseFloat(input.minPrice/100)).toFixed(2) + '+';
         // }
-        return '<span class="bracket-min-price">$<span class="bracket-min-price-num">' + (window.parseFloat(input.minPrice/100)).toFixed(2) + '</span></span>';
+        return '<span class="bracket-min-price">$<span class="bracket-min-price-num">' + (window.parseFloat(input.minPrice/100)).toFixed(2) + '+</span></span>';
     };
 });
